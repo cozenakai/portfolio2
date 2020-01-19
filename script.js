1dab72f2fdd4558685c7fd18a7f46cb20a2757a6
cozenakai <masaki2970@icloud.com>
2020-01-20 01:08:49 +0900

    スライドショー とライブラリの追加（仮）

---
+++ script.js
@@ -0,0 +1,19 @@
+const pics_src = ["img/a.JPG","img/b.JPG","img/c.JPG"];
+let num = -1;
+ 
+ 
+function slideshow_timer(){
+  if (num === 2){
+    num = 0;
+  } 
+  else {
+    num ++;
+  }
+  document.getElementById("mypic").src = pics_src[num];
+}
+ 
+setInterval(slideshow_timer, 1000);
+
+var rythm = new Rythm()
+rythm.setMusic('path/to/sample.mp3')
+rythm.start()
